97f32f45b9eb7c2aa4cfe4402267014de6e3105d
Jucelino <jucelino.qjr@gmail.com>
2017-03-16 12:31:01 -0300

    Criação do webhook criação do app no Facebook

--- app.js
+++ app.js
@@ -13,6 +13,17 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === process.env.VERIFY_TOKEN) {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);
+  }
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
